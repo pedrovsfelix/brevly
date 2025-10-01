8f90bb8885eee38477dbe17e03c3b846863bbef7
pedrovsfelix <seuemail@example.com>
2025-10-01 16:48:25 -0300

    :sparkles: feat: Criando rota de criação do link encurtado.

---
+++ backend/src/http/routes/createLink.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { db } from "../../db/connection";
+import { links } from "../../db/schema";
+import { nanoid } from "nanoid";
+import { sql } from "drizzle-orm";
+
+export async function createLink(app: FastifyInstance) {
+    app.post('/links', async (req, res) => {
+        const createLinkSchema = z.object({
+            originalUrl: z.string().url(),
+            code: z.string().min(3).optional(),
+        });
+
+        const { originalUrl, code: providedCode } = createLinkSchema.parse(req.body);
+        let code = providedCode || nanoid(6);
+
+        while(true) {
+            const [ linkWithSameCode ] = await db.select().from(links).where(sql`code = ${code}`);
+            if(!linkWithSameCode) break;
+            code = nanoid(6);
+        }
+
+        const [ newLink ] = await db.insert(links).values({ originalUrl, code }).returning();
+
+        return res.status(201).send({ linkId: newLink.id, code: newLink.code });
+    });
+}
