1d2a50e0f8a61364b7772aaca1cf69e16d730f57
pedrovsfelix <seuemail@example.com>
2025-10-01 17:15:05 -0300

    :recycle: refactor: Listando as rotas no servidor.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,20 +2,26 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { env } from "./env";
 
+import { createLink } from "./http/routes/createLink";
+import { redirectLink } from "./http/routes/redirectLink";
+import { getLinks } from "./http/routes/getLinks";
+import { deleteLink } from "./http/routes/deleteLink";
+
 const app = fastify();
 
 app.register(cors, {
     origin: '*',
 });
 
-app.get('/', async () => {
-    return { hello: 'Brev.ly API' };
-});
+app.register(getLinks);
+app.register(createLink);
+app.register(deleteLink);
+app.register(redirectLink);
 
 const start = async () => {
     try {
-        await app.listen({ port: env.PORT });
-        console.log(`🚀 HTTP Server is running in http://localhost:3333`);
+        await app.listen({ port: env.PORT, host: '0.0.0.0' });
+        console.log(`🚀 HTTP server running on http://localhost:${env.PORT}`);
     } catch (err) {
         app.log.error(err);
         process.exit(1);
