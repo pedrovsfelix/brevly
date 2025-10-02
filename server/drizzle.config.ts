1b234c52ea4f7eda0bc09e1c089a00f12480f229
pedrovsfelix <pedrovitorsf@gmail.com>
2025-10-01 23:38:48 -0300

    :wrench: chore: AJustando arquivo drizzle

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,15 +1,10 @@
-import 'dotenv/config'; 
 import type { Config } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in the environment variables');
-}
-
 export default {
-  schema: './src/db/schema.ts',
-  out: './src/db/migrations',
-  driver: 'pg',
+  schema: "./src/lib/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
+  out: "./drizzle",
 } satisfies Config;
