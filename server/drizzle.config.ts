d083689182f55435e09c30d015db2dfed2dad014
pedrovsfelix <pedrovitorsf@gmail.com>
2025-10-01 23:15:21 -0300

    :refactor: fix: Ajustando arquivo drizzle

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,15 @@
+import 'dotenv/config'; 
 import type { Config } from "drizzle-kit";
-import { env } from "./src/env";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in the environment variables');
+}
 
 export default {
-    schema: './src/db/schema.ts',
-    out: '.src/db/migrations',
-    driver: 'pg',
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL,
-    },
+  schema: './src/db/schema.ts',
+  out: './src/db/migrations',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL,
+  },
 } satisfies Config;
